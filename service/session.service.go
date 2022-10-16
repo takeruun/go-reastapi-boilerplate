15c634aa22f2acfe4b141951fe414c8aece0a39f
takeru.hashimoto <hstake25@gmail.com>
2022-10-16 02:29:10 +0000

    remove fmt

--- service/session.service.go
+++ service/session.service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/sessions"
@@ -37,8 +36,6 @@ type HTTP struct {
 // GetSession returns a cached session of the given name
 func (service *sessionService) GetSession(ctx context.Context, name string) (*sessions.Session, error) {
 	httpContext := ctx.Value(HTTPKey("http")).(HTTP)
-
-	fmt.Println(httpContext)
 
 	// Ignore err because a session is always returned even if one doesn't exist
 	session, err := service.store.Get(httpContext.R, name)
