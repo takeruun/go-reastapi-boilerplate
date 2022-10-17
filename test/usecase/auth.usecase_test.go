442c202ca1361767a56bfb521a0093dc8049474b
takeru.hashimoto <hstake25@gmail.com>
2022-10-17 23:52:21 +0000

    fix

--- test/usecase/auth.usecase_test.go
+++ test/usecase/auth.usecase_test.go
@@ -38,14 +38,13 @@ func TestSignIn(t *testing.T) {
 	defer setup()
 
 	var (
-		email              = "test@example.com"
-		expectedUser       = entity.User{ID: 1, Email: email, Name: "test", HashPassword: "$2a$10$dipu0jX8IYvy.r.XsecEt.gF4XYsYmBIheZotlxLekYC9UKQWHKe2"}
-		err          error = nil
-		password           = "password"
+		email        = "test@example.com"
+		expectedUser = entity.User{ID: 1, Email: email, Name: "test", HashPassword: "$2a$10$dipu0jX8IYvy.r.XsecEt.gF4XYsYmBIheZotlxLekYC9UKQWHKe2"}
+		password     = "password"
 	)
 
 	t.Run("success", func(t *testing.T) {
-		mockUserRepository.EXPECT().FindByEmail(email).Return(&expectedUser, err)
+		mockUserRepository.EXPECT().FindByEmail(email).Return(&expectedUser, nil)
 		mockCyptoService.EXPECT().ComparePasswords(expectedUser.HashPassword, []byte(password)).Return(true)
 		mockSessionService.EXPECT().SaveSession(gomock.Any(), "userId", expectedUser.ID)
 
@@ -55,13 +54,13 @@ func TestSignIn(t *testing.T) {
 			mockCyptoService,
 		)
 
-		err = authUsecase.SignIn(context.TODO(), &dto.AuthSignInRequestDto{Email: email, Password: password})
+		err := authUsecase.SignIn(context.TODO(), &dto.AuthSignInRequestDto{Email: email, Password: password})
 
 		assert.NoError(t, err)
 	})
 
 	t.Run("If the authentication fails", func(t *testing.T) {
-		mockUserRepository.EXPECT().FindByEmail(email).Return(&expectedUser, err)
+		mockUserRepository.EXPECT().FindByEmail(email).Return(&expectedUser, nil)
 		mockCyptoService.EXPECT().ComparePasswords(expectedUser.HashPassword, []byte(password)).Return(false)
 		mockSessionService.EXPECT().SaveSession(gomock.Any(), "userId", expectedUser.ID)
 
@@ -71,7 +70,7 @@ func TestSignIn(t *testing.T) {
 			mockCyptoService,
 		)
 
-		err = authUsecase.SignIn(context.TODO(), &dto.AuthSignInRequestDto{Email: email, Password: password})
+		err := authUsecase.SignIn(context.TODO(), &dto.AuthSignInRequestDto{Email: email, Password: password})
 
 		assert.Error(t, err)
 		assert.EqualError(t, err, "Authentication Failure")
@@ -83,17 +82,16 @@ func TestSignUp(t *testing.T) {
 	defer setup()
 
 	var (
-		email              = "test@example.com"
-		password           = "password"
-		name               = "test"
-		hashPassword       = "$2a$10$dipu0jX8IYvy.r.XsecEt.gF4XYsYmBIheZotlxLekYC9UKQWHKe2"
-		expectedUser       = entity.User{ID: 1, Email: email, Name: name, HashPassword: hashPassword}
-		err          error = nil
+		email        = "test@example.com"
+		password     = "password"
+		name         = "test"
+		hashPassword = "$2a$10$dipu0jX8IYvy.r.XsecEt.gF4XYsYmBIheZotlxLekYC9UKQWHKe2"
+		expectedUser = entity.User{ID: 1, Email: email, Name: name, HashPassword: hashPassword}
 	)
 
 	t.Run("success", func(t *testing.T) {
-		mockCyptoService.EXPECT().HashAndSalt([]byte(password)).Return(hashPassword, err)
-		mockUserRepository.EXPECT().Create(&entity.User{Email: email, HashPassword: hashPassword, Name: name}).Return(&expectedUser, err)
+		mockCyptoService.EXPECT().HashAndSalt([]byte(password)).Return(hashPassword, nil)
+		mockUserRepository.EXPECT().Create(&entity.User{Email: email, HashPassword: hashPassword, Name: name}).Return(&expectedUser, nil)
 		mockSessionService.EXPECT().SaveSession(gomock.Any(), "userId", expectedUser.ID)
 
 		authUsecase = usecase.NewAuthUsecase(
@@ -102,16 +100,16 @@ func TestSignUp(t *testing.T) {
 			mockCyptoService,
 		)
 
-		err = authUsecase.SignUp(context.TODO(), &dto.AuthSignUpRequestDto{Email: email, Password: password, Name: name})
+		err := authUsecase.SignUp(context.TODO(), &dto.AuthSignUpRequestDto{Email: email, Password: password, Name: name})
 
 		assert.NoError(t, err)
 	})
 
 	t.Run("If hashing fails", func(t *testing.T) {
-		err = errors.New("")
+		err := errors.New("")
 
 		mockCyptoService.EXPECT().HashAndSalt([]byte(password)).Return(hashPassword, err)
-		mockUserRepository.EXPECT().Create(&entity.User{Email: email, HashPassword: hashPassword, Name: name}).Return(&expectedUser, err)
+		mockUserRepository.EXPECT().Create(&entity.User{Email: email, HashPassword: hashPassword, Name: name}).Return(&expectedUser, nil)
 		mockSessionService.EXPECT().SaveSession(gomock.Any(), "userId", expectedUser.ID)
 
 		authUsecase = usecase.NewAuthUsecase(
