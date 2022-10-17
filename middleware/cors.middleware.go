a3e6ddaa5d129182bba1ba3dedbd1bdb147a05d6
takeru.hashimoto <hstake25@gmail.com>
2022-10-17 09:20:53 +0900

    add cors middleware

---
+++ middleware/cors.middleware.go
@@ -0,0 +1,12 @@
+package middleware
+
+import "net/http"
+
+func CorsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		next.ServeHTTP(w, r)
+	})
+}
--- router/router.go
+++ router/router.go
@@ -28,8 +28,8 @@ func NewRouter(appRoute AppRouter, userRoute UserRouter, authRoute AuthRouter, t
 func (r *router) SetRouting() {
 
 	http.HandleFunc("/", r.appRoute.HandleAppRequest)
-	http.Handle("/users/", middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.userRoute.HandleUserRequest))))
-	http.Handle("/todos/", middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.todoRoute.HandleTodoRequest))))
-	http.Handle("/auth/sign_in/", middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.authRoute.SignInRequest))))
-	http.Handle("/auth/sign_up/", middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.authRoute.SignUpRequest))))
+	http.Handle("/users/", middleware.CorsMiddleware(middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.userRoute.HandleUserRequest)))))
+	http.Handle("/todos/", middleware.CorsMiddleware(middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.todoRoute.HandleTodoRequest)))))
+	http.Handle("/auth/sign_in/", middleware.CorsMiddleware(middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.authRoute.SignInRequest)))))
+	http.Handle("/auth/sign_up/", middleware.CorsMiddleware(middleware.SetHttpContextMiddleware(middleware.WriteHeaderMiddleware(http.HandlerFunc(r.authRoute.SignUpRequest)))))
 }
